6d5d244cc3a8475793d41c533b5c9ef65a0c1573
oing0125 <oing0125@gmail.com>
2022-01-27 14:53:58 +0900

    eureka 변수화

--- src/main/java/com/hanhwalife/gateway/Application.java
+++ src/main/java/com/hanhwalife/gateway/Application.java
@@ -2,9 +2,6 @@ package com.hanhwalife.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -12,14 +9,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-//	@Bean
-//	public RouteLocator myRoutes(RouteLocatorBuilder builder){
-//		return builder.routes()
-//				.route(p -> p
-//						.path("/get1")
-//						.filters(f -> f.addRequestHeader("Hello", "World"))
-//						.uri("http://httpbin.org:80"))
-//				.build();
-//	}
 }
